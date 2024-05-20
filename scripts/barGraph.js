24530cfccfef5622d0d8f4552e71fdca0d93acc1
Logan Bondoc <loganbondoc@gmail.com>
2024-05-20 15:43:18 +1000

    added bar graph, and market size csv file

--- scripts/barGraph.js
+++ scripts/barGraph.js
@@ -1,20 +1,39 @@
 const unpack = (data, key) => data.map(row => row[key])
-Plotly.d3.csv("femalepop1.csv", population_data => {
-    const aus = unpack(population_data, 'Australia')
+Plotly.d3.csv("csv/global_market.csv", population_data => {
+    const year = unpack(population_data, 'Year');
+    const market = unpack(population_data, 'Market');
+    console.log(year);
+    console.log(market);
+
+    var trace1 = {
+        x: year,
+        y: market,
+        name: 'Market Size',
+        mode: 'lines+markers',
+        hovertemplate: '<b>Year: </b>%{x}<br>' + '<b>Population: </b>%{y:.2f%}%<extra></extra>',
+    }
+
+    var data = [trace1];
     // const china = unpack(population_data, 'China')
     // const uk = unpack(population_data, 'United Kingdom')
     // const usa = unpack(population_data, 'United States')
     // const germany = unpack(population_data, 'Germany')
-    const year = unpack(population_data, 'Year')
-
-    var trace1 = {
-        x: year,
-        y: aus,
-        name: 'Australia',
-        mode: 'lines+markers',
+    // const year = unpack(population_data, 'Year')
+    // var global_data = [
+    //     {
+    //         x: ['giraffes', 'orangutans', 'monkeys'],
+    //         y: [20, 14, 23],
+    //         type: 'bar'
+    //     }
+    // ]
+    // var trace1 = {
+    //     x: year,
+    //     y: aus,
+    //     name: 'Australia',
+    //     mode: 'lines+markers',
         // hovertemplate: '<b>Country: </b>Australia<br>' + '<b>Year: </b>%{x}<br>' +
         // '<b>Population: </b>%{y:.2f%}%<extra></extra>',
-    }
+    // }
     
     // var trace2 = {
     //     x: year,
@@ -53,37 +72,37 @@ Plotly.d3.csv("femalepop1.csv", population_data => {
     // }
     
     // var data = [trace1, trace2, trace3, trace4, trace5]; 
-    var data = [trace1]; 
+    // var data = [trace1]; 
 
-    var updatemenus = [{
-        type: 'buttons',
-        showactive: false,
-        x: 0,
-        y: 0,
-        xanchor: 'right',
-        yanchor: 'top',
-        direction: 'left',
-        pad: {t: 60, r: 20},
-        buttons: [
-        {
-            label: 'Play',
-            method: 'animate',
-            args: [null, {
-                frame: {duration: 1000},
-                transition: {duration: 500},
-                fromcurrent: true,
-                mode: 'next'
-            }]
-        },
-        {
-            label: 'Pause',
-            method: 'animate',
-            args: [[null], {
-            frame: {duration: 0},
-            mode: 'immediate',
-            transition: {duration: 0}
-            }]
-        },
+    // var updatemenus = [{
+    //     type: 'buttons',
+    //     showactive: false,
+    //     x: 0,
+    //     y: 0,
+    //     xanchor: 'right',
+    //     yanchor: 'top',
+    //     direction: 'left',
+    //     pad: {t: 60, r: 20},
+    //     buttons: [
+    //     {
+    //         label: 'Play',
+    //         method: 'animate',
+    //         args: [null, {
+    //             frame: {duration: 1000},
+    //             transition: {duration: 500},
+    //             fromcurrent: true,
+    //             mode: 'next'
+    //         }]
+    //     },
+    //     {
+    //         label: 'Pause',
+    //         method: 'animate',
+    //         args: [[null], {
+    //         frame: {duration: 0},
+    //         mode: 'immediate',
+    //         transition: {duration: 0}
+    //         }]
+    //     },
         // {
         // args: ['mode', 'lines+markers'],
         // label: 'lines & markers',
@@ -100,7 +119,7 @@ Plotly.d3.csv("femalepop1.csv", population_data => {
         //     label: 'markers',
         //     method: 'restyle'
         // }
-            ],
+            // ],
             // button approach
                 // direction: 'left',
                 // pad: {'r': 200, 't': 10},
@@ -119,18 +138,18 @@ Plotly.d3.csv("femalepop1.csv", population_data => {
             // xanchor: 'middle',
             // y: 2,
             // yanchor: 'top'
-}]
+// }]
 
-var frames = [{
-    name: 'lines',
-    data: [{mode: 'lines'}]
-    }, {
-    name: 'markers',
-    data: [{mode: 'markers'}]
-    }, {
-    name: 'lines+markers',
-    data: [{mode: 'lines+markers'}]
-}];
+// var frames = [{
+//     name: 'lines',
+//     data: [{mode: 'lines'}]
+//     }, {
+//     name: 'markers',
+//     data: [{mode: 'markers'}]
+//     }, {
+//     name: 'lines+markers',
+//     data: [{mode: 'lines+markers'}]
+// }];
 
 var layout = {
     // annotations: [
@@ -148,36 +167,36 @@ var layout = {
     //     ay: -40
     //     }
     // ],
-    // hovermode: 'closest',
-    // hoverlabel: {
-    //     bgcolor: "#FFF",
-    //     bordercolor: "#003166",
-    //     font: {
-    //     family: 'Courier New, monospace',
-    //     size: 18,
-    //     color: '#7f7f7f'
-    //     }
-    // },
-    updatemenus: updatemenus,
-    sliders: [{
-        pad: {t: 30},
-        currentvalue: {
-            xanchor: 'right',
-            prefix: 'Mode: ',
-            font: {
-                color: '#888',
-                size: 20
-            }
-        },
-        steps: [{
-            label: 'Lines',
-            method: 'animate',
-            args: [['lines'], {
-            mode: 'immediate',
-            transition: {duration: 0}
-        }]
-    }],
-}],
+    hovermode: 'closest',
+    hoverlabel: {
+        bgcolor: "#FFF",
+        bordercolor: "#003166",
+        font: {
+        family: 'Courier New, monospace',
+        size: 18,
+        color: '#7f7f7f'
+        }
+    },
+//     updatemenus: updatemenus,
+//     sliders: [{
+//         pad: {t: 30},
+//         currentvalue: {
+//             xanchor: 'right',
+//             prefix: 'Mode: ',
+//             font: {
+//                 color: '#888',
+//                 size: 20
+//             }
+//         },
+//         steps: [{
+//             label: 'Lines',
+//             method: 'animate',
+//             args: [['lines'], {
+//             mode: 'immediate',
+//             transition: {duration: 0}
+//         }]
+//     }],
+// }],
 }
 
 
